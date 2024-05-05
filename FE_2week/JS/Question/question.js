b83f501d216e5eb8e14838169ae1e7279d3d57b9
taejun0 <xownswns@naver.com>
2024-05-05 22:52:22 +0900

    hw finish

--- FE_2week/JS/Question/question.js
+++ FE_2week/JS/Question/question.js
@@ -5,11 +5,19 @@ const arr2 = [1, [2, 3, [4, 5, 6], 7], [8, 9]];
 
 // Q1-1. arr에서 3출력 (Hint:인덱싱은 0부터 시작!)
 
+console.log(arr[2]);
+
 // Q1-2. arr에서 [1,2,3]출력 (Hint:Array.js에서 slice함수를 사용해볼까요?)
+
+console.log(arr.slice(0, 3));
 
 // Q1-3. arr2에서 [8, 9]출력 (Hint:배열 안에 원소가 배열일 뿐!)
 
+console.log(arr2[2]);
+
 // Q1-4. arr2에서 [4, 5, 6]출력 (Hint:원소안 원소!)
+
+console.log(arr2[1][2]);
 
 console.log("======= 문제 구분선 =======");
 
@@ -47,11 +55,19 @@ const obj = {
 
 // Q2-1. 멋쨍이사자의 나이를 obj객체로부터 출력하기 (Hint:obj.~~)
 
+console.log(obj.age);
+
 // Q2-2. 멋쨍이사자의 Computer 학점(A+)을 obj객체로부터 출력하기
+
+console.log(obj.gpa.Computer);
 
 // Q2-3. 아기사자 나이(21)를 obj객체로부터 출력하기 (Hint:obj안에 배열)
 
+console.log(obj.friends[0].age);
+
 // Q2-4. 코딩사자가 수강한 강의 중에서 "종합설계"를 obj객체로부터 출력하기
+
+console.log(obj.friends[1].lecture[1][2]);
 
 console.log("======= 문제 구분선 =======");
 
@@ -61,7 +77,11 @@ const arr3 = [1, 2, 3, 4, 5, 6];
 
 // Q3-1. map함수와 arr3을 활용하여 [10,20,30,40,50,60]을 출력하시오.
 
+console.log(arr3.map(function(n){return n*10}));
+
 // Q3-2. filter함수와 arr3을 활용하여 [1,3,5]출력하기 (Hint:홀수와 짝수 구분은 "%2"를 활용)
+
+console.log(arr3.filter(function(n){return n%2 == 1}))
 
 console.log("======= 문제 구분선 =======");
 
@@ -80,6 +100,17 @@ console.log(isLeap(2020));  -> 윤년이 맞습니다.
 console.log(isLeap(2021));  -> 윤년이 아닙니다.
 */
 
+function isLeap(year) {
+  if (year % 4 === 0 && year % 100 !== 0 || year % 400 === 0){
+    return ("윤년이 맞습니다.");
+  }
+  else
+    return ("윤년이 아닙니다.");
+}
+
+console.log(isLeap(2020));
+console.log(isLeap(2021));
+
 console.log("======= 문제 구분선 =======");
 
 // Q5 반복문 연습1
@@ -93,6 +124,11 @@ console.log("Q5 반복문 연습문제1");
 *****
 (Hint:for문 or while문 사용 + console.log("*")사용)
 */
+
+for (let i=1; i<=5; i++) {
+  console.log("*".repeat(i));
+}
+  
 
 console.log("======= 문제 구분선 =======");
 
@@ -108,6 +144,10 @@ console.log("Q6 반복문 연습문제2");
 (Hint:console.log(" ")로 빈칸 표현)
 */
 
+for (let i=1; i<=5; i++) {
+  console.log(" ".repeat(5-i) + "*".repeat(i));
+}
+
 console.log("======= 문제 구분선 =======");
 
 // Q7 반복문 연습3
@@ -121,5 +161,9 @@ console.log("Q7 반복문 연습문제3");
 *********
 (Hint:이중for문 활용)
 */
+
+for (let i=1; i<=10; i=i+2) {
+  console.log(" ".repeat((10-i)/2) + "*".repeat(i));
+}
 
 console.log("고생하셨습니다!!");
