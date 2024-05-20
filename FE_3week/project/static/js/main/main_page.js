648399b44d91fe793842de0f6ec45437b7a78a14
taejun0 <xownswns@naver.com>
2024-05-20 22:43:29 +0900

    FE_3week hw cear

--- FE_3week/project/static/js/main/main_page.js
+++ FE_3week/project/static/js/main/main_page.js
@@ -1,1 +1,98 @@
 /* main page - JS */
+
+const checkboxes = document.querySelectorAll("input[type='checkbox'][name='category_check_group']");
+const reset_btn = document.getElementById("reset_btn");
+const skincare = document.getElementById("skincare");
+const luxury = document.getElementById("luxury");
+const snack = document.getElementById("snack");
+const total_item = document.getElementById("total_item");
+const filter = document.getElementById("filter");
+const skincare_btn = document.getElementById("skincare_btn");
+const luxury_btn = document.getElementById("luxury_btn");
+const snack_btn = document.getElementById("snack_btn");
+
+
+// 카테고리 표시
+const category_display = () => {
+  let total = 0;
+  if (skincare.checked == false && luxury.checked == false && snack.checked == false){
+    filter.style.display = "none";
+    reset_btn.style.color = "#dddddd";
+    total_item.textContent = "총 508건";
+  }
+  else{
+    document.getElementById("filter").style.display = "";
+    document.getElementById("reset_btn").style.color = "#999999"
+    for (var i = 0; i<checkboxes.length; i++){
+      if (skincare.checked == false) {
+        document.getElementById("filter_skincare").style.display = "none";
+      }
+      if (luxury.checked == false) {
+        document.getElementById("filter_luxury").style.display = "none";
+      }
+      if (snack.checked == false) {
+        document.getElementById("filter_snack").style.display = "none";
+      }
+      if (skincare.checked == true) {
+        document.getElementById("filter_skincare").style.display = "";
+        total += 68/3;
+      }
+      if (luxury.checked == true) {
+        document.getElementById("filter_luxury").style.display = "";
+        total += 24/3;
+      }
+      if (snack.checked == true) {
+        document.getElementById("filter_snack").style.display = "";
+        total += 28/3;
+      }
+    }
+  }
+  total = Math.round(total);
+  total_item.textContent = "총 "+total+"건";
+}
+
+// 카테고리 초기화 버튼
+const reset = () => {
+  checkboxes.forEach((checkbox) => {
+    checkbox.checked = false;
+  })
+  filter.style.display = "none";
+  reset_btn.style.color = "#dddddd";
+  total_item.textContent = "총 508건";
+}
+
+// filter 삭제 버튼
+const category_delete_skincare = () => {
+  skincare.checked = false;
+}
+
+const category_delete_luxury = () => {
+  luxury.checked = false;
+}
+
+const category_delete_snack = () => {
+  snack.checked = false;
+}
+
+
+// addEventListener 파트
+skincare.addEventListener('change', category_display);
+luxury.addEventListener('change', category_display);
+snack.addEventListener('change', category_display);
+reset_btn.addEventListener('click', reset);
+skincare_btn.addEventListener('click', category_delete_skincare);
+skincare_btn.addEventListener('click', category_display);
+luxury_btn.addEventListener('click', category_delete_luxury);
+luxury_btn.addEventListener('click', category_display);
+snack_btn.addEventListener('click', category_delete_snack);
+snack_btn.addEventListener('click', category_display);
+
+
+/*
+checkboxes.forEach((checkbox) => {
+  checkbox.addEventListener("change", (event) => {
+    for(var i = 0; i<checkboxes.length; i++){
+    }
+  });
+});
+*/
--- FE_3week/project/static/js/main/product_detail_page.js
+++ FE_3week/project/static/js/main/product_detail_page.js
@@ -1,1 +1,35 @@
 /* product detail page - JS */
+
+const product_amount = document.getElementById("product_amount");
+const product_amount_plus_btn = document.getElementById("product_amount_plus_btn");
+const product_amount_minus_btn = document.getElementById("product_amount_minus_btn");
+const final_chargement = document.getElementById("final_chargement");
+const product_price = document.getElementById("product_price").textContent.replace(' 원', '');
+
+const product_amount_plus = () => {
+  product_amount.textContent = parseInt(product_amount.textContent) + 1;
+  calc_product();
+}
+
+const product_amount_minus = () => {
+  if(parseInt(product_amount.textContent) <= 0) {
+    product_amount.textContent = 0;
+  }
+  else {
+    product_amount.textContent = parseInt(product_amount.textContent) - 1;
+  }
+  calc_product();
+}
+
+const calc_product = () => {
+  if(parseInt(product_amount.textContent) <= 0) {
+    final_chargement.textContent = "0 원";
+  }
+  else {
+    final_chargement.textContent = (parseInt(product_amount.textContent) * parseInt(product_price)) + " 원";
+  }
+}
+
+product_amount_plus_btn.addEventListener('click', product_amount_plus);
+product_amount_minus_btn.addEventListener('click', product_amount_minus);
+
